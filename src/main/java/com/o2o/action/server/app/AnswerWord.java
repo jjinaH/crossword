6b6cfef35b40b5df6a29b42065bebc0651e7fff8
zheng2x2 <zheng@>
2020-10-22 14:33:06 +0900

    change hint data type (from str[] to str)

--- src/main/java/com/o2o/action/server/app/AnswerWord.java
+++ src/main/java/com/o2o/action/server/app/AnswerWord.java
@@ -6,29 +6,45 @@ import java.util.Stack;
 // 정답 클래스
 public class AnswerWord implements Comparable, Serializable {
 
-    private String Answer; // 정답 문자
-    private String[] Hints; // 정답의 힌트 배열
-    private Stack<String> HintsStack; // 정답의 힌트사용 카운트를 위한 힌트 스택
-    private String[] AnswerPoint; // 정답 알파벳의 좌표 배열
-
-    public AnswerWord(String _word, String[] _hints) {
-        Answer = _word;
-        Hints = _hints.clone();
-        HintsStack = new Stack<String>();
-        HintsStack.push(Character.toString(Answer.charAt(0)));
-        String LenSt = "";
-        for(int i=0; i< Answer.length(); i++)
-            LenSt +="_ ";
-
-        HintsStack.push(LenSt);
-
-        HintsStack.push(Hints[0]);
-        // row좌표 col좌표 이므로 *2의 배열 자리 선정
-        AnswerPoint = new String[Answer.length()];
+    private String answer; // 정답 문자
+    public String getAnswer() {
+        return answer;
     }
+
+    private String[] AnswerPoint; // 정답 알파벳의 좌표 배열
+    public String[] getAlphabetPoint() {
+        return AnswerPoint;
+    } // 정답의 알파벳 위치 배열 반환
+
+    private String hint; // private String[] Hints; // 정답의 힌트 배열 => 배열대신 Str으로 변경, 추후 DB의 힌트가 늘어날경우 배열로 사용
+    private Stack<String> hintsStack; // 정답의 힌트사용 카운트를 위한 힌트 스택
+
+    public String useHint() { // 해당 단어의 힌트스택에서 힌트 뺴기
+        if(hintsStack.empty()) return "noHint"; // 만약 스택이 비었으면 noHint 출력
+        String hint = hintsStack.pop();
+        return hint;
+    }
+
+    public AnswerWord(String _word, String _hint) { //String[] _hints) {
+        answer = _word;
+        hint = _hint; //_hints.clone();
+        hintsStack = new Stack<String>();
+
+        hintsStack.push(Character.toString(answer.charAt(0))); //세번째 힌트 :: 첫번째 글자
+        String LenSt = ""; //두번째 힌트 :: 글자수
+        for(int i=0; i< answer.length(); i++) LenSt +="_ ";
+        hintsStack.push(LenSt);
+        hintsStack.push(hint); //Hints[0]); //첫번째 힌트 :: DB의 힌트 그대로
+
+        AnswerPoint = new String[answer.length()]; // row좌표 col좌표 이므로 *2의 배열 자리 선정
+    }
+
+    /**
+     * Board 생성알고리즘에 필요한 함수들 ...
+     */
     // 해당 정답의 알파벳 위치 기록
-    public void SetAlphabetPoint(int _alphabetindex, int _row, int _col) {
-        if(_alphabetindex>=Answer.length()) {
+    public void setAlphabetPoint(int _alphabetindex, int _row, int _col) {
+        if(_alphabetindex>=answer.length()) {
             System.out.println("인덱스가 단어의 길이를 벗어났습니다.");
             return;
         }
@@ -36,29 +52,11 @@ public class AnswerWord implements Comparable, Serializable {
         // AnswerPoint[_alphabetindex*2] = _row;
         AnswerPoint[_alphabetindex] = pointstring;
     }
-    // 정답의 알파벳 위치 배열 반환
-    public String[] GetAlphabetPoint() {
-        return AnswerPoint;
+    public int getAnswerLength() { // 정답 길이 반환
+        return answer.length();
     }
-
-    // 해당 힌트스택에서 힌트 뺴기
-    public String useHint() {
-        if(HintsStack.empty()) // 만약 스택이 비었으면 noHint 출력
-            return "noHint";
-        String hint = HintsStack.pop();
-        return hint;
-    }
-    // 정답 길이 반환
-    public int getAnswerLength() {
-        return Answer.length();
-    }
-    // 정답 String 내부 char 반환
-    public char getAnswerChar(int _index) {
-        return Answer.charAt(_index);
-    }
-    // 정답 반환
-    public String getAnswer() {
-        return Answer;
+    public char getAnswerChar(int _index) { //answer str의 각 alphabet return
+        return answer.charAt(_index);
     }
 
     @Override
