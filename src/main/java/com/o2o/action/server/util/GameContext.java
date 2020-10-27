4ff213fa0f3818d1832641a6ce0ef562c8207551
zheng2x2 <zheng@>
2020-10-27 18:04:15 +0900

    add Context to get properties values

---
+++ src/main/java/com/o2o/action/server/util/GameContext.java
@@ -0,0 +1,52 @@
+package com.o2o.action.server.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+//@Component
+@Configuration
+//@PropertySources({
+    @PropertySource("classpath:/static/properties/game.properties")
+//    @PropertySource("classpath:db.properties")
+//})
+public class GameContext { //AppConfig {
+    @Autowired
+    private Environment environment;
+
+    public int getSize(int level) {
+        return Integer.parseInt(environment.getProperty("level" + level + ".size"));
+    }
+
+    public int getAnswerCnt(int level) {
+        return Integer.parseInt(environment.getProperty("level" + level + ".answer"));
+    }
+
+    public int getTimeLimit(int level, String difficulty) {
+        int timeLimit = Integer.parseInt(environment.getProperty("level" + level + ".duration"));
+
+        if("easy".equals(difficulty)) return timeLimit;
+        else if("medium".equals(difficulty)) return timeLimit - 5;
+        else if("hard".equals(difficulty)) return timeLimit - 10;
+
+        return timeLimit; //TODO default?
+    }
+    public int getBettingCoins(String difficulty) {
+//        System.out.println("environment.getProperty(\"betcoin.\" + difficulty) >>> " + environment.getProperty("betcoin." + difficulty));
+        return Integer.parseInt(environment.getProperty("betcoin." + difficulty));
+    }
+    public int getWinExp(int level, String difficulty){
+        int winExp = Integer.parseInt(environment.getProperty("level" + level + ".exp"));
+        if("easy".equals(difficulty)) return winExp;
+        else if("medium".equals(difficulty)) return winExp * 2;
+        else if("hard".equals(difficulty)) return winExp * 3;
+
+        return winExp; //TODO default?
+    }
+
+    public int getLevelUpExp(int currLevel) {
+        return Integer.parseInt(environment.getProperty("level" + currLevel + ".levelupexp"));
+    }
+
+}
